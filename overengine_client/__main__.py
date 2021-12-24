e56f2e7ec0605efb16f2abd932c5bcfd070f045e
jok4r <opensuse12@gmail.com>
2021-12-24 22:29:01 +0300

    Переместил main в main

---
+++ overengine_client/__main__.py
@@ -0,0 +1,29 @@
+from overengine_client import OverEngineClient
+import signal
+import datetime
+import asyncio
+import sys
+
+
+def signal_handler(sig, frame):
+    print("Pressed Ctrl+C, exiting...")
+    # global script_run
+    # script_run = False
+    sys.exit(0)
+
+
+print("Running script on %s" % datetime.datetime.now())
+signal.signal(signal.SIGINT, signal_handler)
+print('Press Ctrl+C for exit')
+oec = OverEngineClient(verbose=False, debug=False)
+input_text = 'Select mode (m - message, df - download file, uf - upload file, vfs - verify file sign)'
+main_mode = input(input_text + ' or d, v, dv - debug, verbose: ')
+if main_mode in ['d', 'v', 'dv']:
+    if main_mode in ['d', 'dv']:
+        oec.debug = True
+    if main_mode in ['v', 'dv']:
+        oec.verbose = True
+    main_mode = input(f'{input_text}: ')
+    if main_mode not in ['m', 'df', 'uf', 'vfs']:
+        raise ValueError(f"Incorrect mode: {main_mode}")
+asyncio.run(oec.manual_send(main_mode))
