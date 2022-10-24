3900bacd466813cdcd35f7d046f6858a15f42428
jok4r <opensuse12@gmail.com>
2022-10-25 01:56:38 +0300

    Added server config path for Windows

--- ovtp/server/config.py
+++ ovtp/server/config.py
@@ -2,18 +2,22 @@ import ovcfg
 import os
 
 
-config_dirs = [
-    [['/', 'var', 'lib'], []],
-    [[os.path.expanduser("~"), '.local'], ['share']]
-]
+if os.name == 'posix':
+    config_dirs = [
+        [['/', 'var', 'lib'], []],
+        [[os.path.expanduser("~"), '.local'], ['share']]
+    ]
 
-
-for config_dir in config_dirs:
-    if os.access(os.path.join(*config_dir[0]), os.W_OK):
-        config_path = os.path.join(*config_dir[0], *config_dir[1], 'ovtp')
-        break
+    for config_dir in config_dirs:
+        if os.access(os.path.join(*config_dir[0]), os.W_OK):
+            config_path = os.path.join(*config_dir[0], *config_dir[1], 'ovtp')
+            break
+    else:
+        raise RuntimeError("Can't create ovcrypt config directory")
+elif os.name == 'nt':
+    config_path = os.path.join(os.getenv('APPDATA'), 'ovtp')
 else:
-    raise RuntimeError("Can't create ovcrypt config directory")
+    raise RuntimeError(f'Unknown platform: {os.name}')
 
 
 sc = {
